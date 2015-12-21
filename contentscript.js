779fb5814b733fac4ecaa9830b1601ef77f8a0bb
Jin Zhou <ferryzhou@gmail.com>
2015-12-20 18:55:50 -0800

    only show ebay price when it's lower than amazon price

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,7 @@
 
 $(function() {
-  var template = _.template('<a class="title" href="<%= ListingUrl %>" title="<%= Title %>" target="_blank"> <%= CurrentPrice %> </a>');
-  
+  var template = _.template('<a style="color: red; text-decoration: underline" href="<%= ListingUrl %>" title="<%= Title %>" target="_blank">(Ebay Price: $<%= CurrentPrice %>) </a>');
+
   function get_items_html(ebay_items) {
 		if (!ebay_items) {
 			console.error("no results");
@@ -13,8 +13,12 @@ $(function() {
     }
     return items.join('\n');
   }
-  
+
   var item_title = $('#productTitle').text();
+	if (!item_title) {
+		console.error("cannot find title!");
+		return;
+	}
 	console.log("title: " + item_title)
 	var ss = item_title.split(" ");
 	console.log("ss: " + JSON.stringify(ss))
@@ -25,10 +29,42 @@ $(function() {
 	console.log('query ' + url)
   $.getJSON(url, function(resp) {
 		console.log('got ' + JSON.stringify(resp));
-    var str = '<span style="font-size: 12px; margin-top:3px;">' 
+		if (!resp.Items || resp.Items.length < 1) {
+			console.error("no results");
+			return;
+		}
+		var amazon_price_text = $("#priceblock_saleprice").text() || $("#priceblock_ourprice").text();
+		if (!amazon_price_text) {
+			console.error("cannot find amazon price.");
+			return;
+		}
+		var amazon_price = parseFloat(amazon_price_text.substring(1));
+		if (!amazon_price) {
+			console.error("failed to parse amazon price: " + amazon_price_text);
+			return;
+		}
+		console.log("amazon price: " + amazon_price);
+
+		var ebay_price_str = resp.Items[0].CurrentPrice;
+		if (!ebay_price_str) {
+			console.log("ebay price not found!");
+			return;
+		}
+
+		var ebay_price = parseFloat(ebay_price_str);
+		if (!ebay_price) {
+			console.log("failed to parse ebay price: " + ebay_price_str);
+			return;
+		}
+
+		console.log("ebay price: " + ebay_price);
+
+		if (amazon_price > ebay_price) {
+			var str = '<span>' 
                 + get_items_html(resp.Items) + '</span>'
-    $('#title').append(str);
-  }); 
+			$('#title').append(str);
+		}
+  });
 });
 
 
